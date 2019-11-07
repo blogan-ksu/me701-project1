2d12f7f30cff24d22a205bb6adfff6768565b451
Ben Logan <blogan1125@ksu.edu>
2019-11-07 15:55:19 -0600

    Finished the project solution, need to implement unit tests

--- Project_solution.py
+++ Project_solution.py
@@ -16,13 +16,16 @@ Sy = 490.0 # MPa
 F_max = 15000.0 # Newtons
 F_min = 5000.0 # Newtons
 
+w = 30.0 # mm
+h = 10.0 # mm
+
 n = 1.2 # Safety Factor
 
 #%%
 # Loading Factors:
 C_load = 0.7
 
-A_95 = 0.05* 30 * 10 # mm^2
+A_95 = 0.05* w * h # mm^2
 d_rect_area = sp.sqrt(A_95/0.076) # mm
 C_size = 1.189*d_rect_area**(-0.097)
 
@@ -45,29 +48,33 @@ Se = get_Se(Se_prime, C_load, C_size, C_surface, C_reliab, C_temp) # MPa
 F_m = (F_max + F_min) / 2 # Newtons
 F_a = (F_max - F_min) / 2 # Newtons
 
-def get_sigma_m(d, F_m):
+def get_sigma_m(d, F_m, w, h):
     '''Returns sigma m'''
-    return F_m/(300 - 10 * d)
+    return F_m/(w*h - h*d)
 
 def get_sigma_a(d, F_a):
     '''Returns sigma a'''
-    return F_a/(300 - 10 * d)
+    return F_a/(w*h - h*d)
 
 #%%
 # Set up Minimize Equations/Constraints:
-def diameter(d, Se, Sut, F_a, F_m, n):
-    x = d/30
-    sol = 30 - ( (Se + Sut) / (10*n*(F_a + F_m)) ) * \
-          ( 1 / (3 - 3.14*(x) + 3.667*(x)**2 - 1.527*(x)**3))
+def diameter(d, Se, Sut, F_a, F_m, n, w):
+    x = d/w
+    k_t = (3 - 3.14*(x) + 3.667*(x)**2 - 1.527*(x)**3)
+    sol = 30 - k_t*n*0.1*( F_a/Se + F_m/Sut )
     return sol
 
 
-objective = lambda x: - diameter(x, Se, Sut, F_a, F_m, n)
-constraint1 = lambda x: 30 - x
-constraint2 = lambda x: (Sy / n) - (get_sigma_m(x, F_m) + get_sigma_a(x, F_a))
+objective = lambda x: - diameter(x, Se, Sut, F_a, F_m, n, w)
 
+constraint1 = lambda x: w - x
+constraint2 = lambda x: (Sy / n) - (F_max / (w*h - h*x)) * \
+                        (3 - 3.14*(x/w) + 3.667*(x/w)**2 - 1.527*(x/w)**3)
+constraint3 = lambda x: diameter(x, Se, Sut, F_a, F_m, n, w) - x
+                        
 constraints = [{'type': 'ineq', 'fun': constraint1},
-               {'type': 'ineq', 'fun': constraint2}]
+               {'type': 'ineq', 'fun': constraint2},
+               {'type': 'eq', 'fun': constraint3}]
 
 d = 10.0 # mm (Initial Value)
 
