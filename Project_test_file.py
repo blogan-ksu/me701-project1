edd3dba69d4c7e16d69f89d3a66e8ee29dcfef7e
Ben Logan <blogan1125@ksu.edu>
2019-11-08 16:53:48 -0600

    Cleaned up Project_test_file.py

--- Project_test_file.py
+++ Project_test_file.py
@@ -3,7 +3,8 @@
 
 import unittest
 from Project_solution import get_Se, answer, diameter, get_sigma_m,\
-                             get_sigma_a, get_sigma_max
+                             get_sigma_a, get_sigma_max, \
+                             Sut, Sy, F_m, F_a, w, h, n, Se
 
 class Test_variables(unittest.TestCase):
     
@@ -16,15 +17,15 @@ class Test_variables(unittest.TestCase):
     
     def test_get_sigma_m(self):
         '''Tests that get_sigma_m returns correct mean stress.'''
-        self.assertAlmostEqual(get_sigma_m(10, 10000, 30, 10), 50)
+        self.assertAlmostEqual(get_sigma_m(10, F_m, w, h), 50)
     
     def test_get_sigma_a(self):
         '''Tests that get_sigma_a returns correct stress amplitude.'''
-        self.assertAlmostEqual(get_sigma_a(10, 5000, 30, 10), 25)
+        self.assertAlmostEqual(get_sigma_a(10, F_a, w, h), 25)
         
     def test_get_sigma_max(self):
         '''Tests that get_sigma_max returns correct stress amplitude.'''
-        self.assertAlmostEqual(get_sigma_max(10, 10000, 5000, 30, 10), 75)
+        self.assertAlmostEqual(get_sigma_max(10, F_m, F_a, w, h), 75)
     
     def test_valid_diameter(self):
         ''' 
@@ -33,9 +34,8 @@ class Test_variables(unittest.TestCase):
         '''
         
         x = float(answer.x)
-        Se = get_Se(295, 0.7, 0.9201547826571058, 0.9186173970642588, 0.814, 1)
         self.assertAlmostEqual(x,\
-                               diameter(x, Se, 590, 5000, 10000, 1.2, 30, 10),\
+                               diameter(x, Se, Sut, F_a, F_m, n, w, h),\
                                places=4)
         
     def test_for_yielding(self):
@@ -43,22 +43,22 @@ class Test_variables(unittest.TestCase):
         This tests to make sure that yielding will not occur at the diameter
         that has been sovled for.
         '''
-        x = float(answer.x) / 30.0
+        x = float(answer.x) / w
         k_t = (3 - 3.14*(x) + 3.667*(x)**2 - 1.527*(x)**3)
-        sigma_max = get_sigma_max(float(answer.x), 10000, 5000, 30, 10)
-        A = 300-10*float(answer.x)
-        self.assertLess(k_t*sigma_max/A, 490)
+        sigma_max = get_sigma_max(float(answer.x), F_m, F_a, w, h)
+        A = w*h - h*float(answer.x)
+        self.assertLess(k_t*sigma_max/A, Sy)
 
     def test_for_safety_factor(self):
         '''
         This tests to make sure that the maximum stress at the solved diameter
         is less than the allowable strength accounted for by the safety factor.
         '''
-        x = float(answer.x) / 30.0
+        x = float(answer.x) / w
         k_t = (3 - 3.14*(x) + 3.667*(x)**2 - 1.527*(x)**3)
-        sigma_max = get_sigma_max(float(answer.x), 10000, 5000, 30, 10)
-        A = 300-10*float(answer.x)
-        self.assertLess(k_t*sigma_max/A, 490/1.2)
+        sigma_max = get_sigma_max(float(answer.x), F_m, F_a, w, h)
+        A = w*h - h*float(answer.x)
+        self.assertLess(k_t*sigma_max/A, Sy/n)
         
         
 if __name__ == "__main__":
